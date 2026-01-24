5e135f7ea425bc85977ba43ef21f4c38ea927593
stargazer <163490937+uss-stargazer@users.noreply.github.com>
2026-01-24 01:00:09 -0500

    chore: rename outDir to dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ import tseslint from 'typescript-eslint';
 // You should change it to your needs following the documentation.
 export default tseslint.config(
   {
-    ignores: ['**/build/**', '**/tmp/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/tmp/**', '**/coverage/**'],
   },
   eslint.configs.recommended,
   eslintConfigPrettier,
